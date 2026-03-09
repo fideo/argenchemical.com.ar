7eb351d9704ed47532de3fa778ba5079147e227f
Federico Mazzei <federicomazzei@gmail.com>
2026-03-09 13:40:08 -0300

    commit desde raspi

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -115,9 +115,9 @@ $table_prefix = getenv_docker('WORDPRESS_TABLE_PREFIX', 'wp_');
  */
 //define( 'WP_DEBUG', !!getenv_docker('WORDPRESS_DEBUG', '') );
 
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
-define( 'WP_DEBUG_DISPLAY', false );
+#define( 'WP_DEBUG', true );
+#define( 'WP_DEBUG_LOG', true );
+#define( 'WP_DEBUG_DISPLAY', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
--- wordpress/wp-content/themes/argenchemical/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="site-footer">
-    <p>&copy; <?php echo date('Y'); ?> Argenchemical - Todos los derechos reservados.</p>
-</footer>
-<?php wp_footer(); ?>
-</body>
-</html>
